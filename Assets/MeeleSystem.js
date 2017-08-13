3274684f4f862b7d66bc58435ff2aada3d00c8e2
Joy <njoy@BANL142a7b3f3.local>
2017-08-13 22:17:50 +0530

    Initial Commit

---
+++ Assets/MeeleSystem.js
@@ -0,0 +1,17 @@
+﻿#pragma strict
+
+
+var theDammage : int = 50;
+var distance : float;
+
+function Update() {
+
+	if(Input.GetButtonDown("Fire1")) {
+		var hit : RaycastHit;
+		if(Physics.Raycast(transform.position, transform.TransformDirection(Vector3.forward), hit)) {
+
+			distance = hit.distance;
+			hit.transform.SendMessage("Apply Damage", theDammage, SendMessageOptions.DontRequireReceiver);
+		}
+	}
+} 
