c467219df33a74ac0ba91126d7fad718185f56a0
Joy <njoy@BANL142a7b3f3.local>
2017-08-14 22:07:29 +0530

    Trying to animate

---
+++ Assets/EnemyLogic.js
@@ -0,0 +1,16 @@
+﻿#pragma strict
+
+var health : int = 100;
+
+function Update(){
+	if(health <= 0)
+		Dead();
+}
+
+function ApplyDamage(theDamage : int) {
+	health -= theDamage;
+}
+
+function Dead() {
+	Destroy (gameObject) ;
+}
--- Assets/MeeleSystem.js
+++ Assets/MeeleSystem.js
@@ -3,15 +3,18 @@
 
 var theDammage : int = 50;
 var distance : float;
+var maxDistance : float = 1.5;
+
 
 function Update() {
-
 	if(Input.GetButtonDown("Fire1")) {
+		GetComponent.<Animation>().Play();
 		var hit : RaycastHit;
 		if(Physics.Raycast(transform.position, transform.TransformDirection(Vector3.forward), hit)) {
-
 			distance = hit.distance;
-			hit.transform.SendMessage("Apply Damage", theDammage, SendMessageOptions.DontRequireReceiver);
+			if(distance < maxDistance) {
+				hit.transform.SendMessage("ApplyDamage", theDammage, SendMessageOptions.DontRequireReceiver);
+			}
 		}
 	}
 } 
