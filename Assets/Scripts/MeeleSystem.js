065339861d499060e2b7bc05ba3f65cbd82d301d
Joy <njoy@BANL142a7b3f3.local>
2017-08-15 16:06:41 +0530

    More animation - Sprint not working

---
+++ Assets/Scripts/MeeleSystem.js
@@ -0,0 +1,29 @@
+﻿#pragma strict
+
+
+var theDammage : int = 50;
+var distance : float;
+var maxDistance : float = 2.5;
+var Weapon : Transform;
+
+function Update() {
+	if(Input.GetButtonDown("Fire1")) {
+		Weapon.GetComponent.<Animation>().Play("MeeleAttack");
+		var hit : RaycastHit;
+		if(Physics.Raycast(transform.position, transform.TransformDirection(Vector3.forward), hit)) {
+			distance = hit.distance;
+			if(distance < maxDistance) {
+				hit.transform.SendMessage("ApplyDamage", theDammage, SendMessageOptions.DontRequireReceiver);
+			}
+		}
+	}
+	if(Weapon.GetComponent.<Animation>().isPlaying == false)
+		Weapon.GetComponent.<Animation>().CrossFade("Idle");
+
+	if(Input.GetKey(KeyCode.LeftShift))
+		Weapon.GetComponent.<Animation>().CrossFade("Sprint");
+
+	if(Input.GetKeyUp(KeyCode.LeftShift))
+		Weapon.GetComponent.<Animation>().CrossFade("Idle");
+
+} 
